2e5688166d6fe01422517230ba8695abf7675024
Ken Chigozie <kenronkwo@gmail.com>
2024-05-14 20:44:41 +0100

    blog

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,35 +1,43 @@
-'use client'
+"use client";
 import React from "react";
 import { useTypewriter, Cursor } from "react-simple-typewriter";
 
 import Container from "./Container";
 
-
-
-
-
 const Hero = () => {
-const [typeEffect] = useTypewriter({
-  words: ['Medical Doctor', 'Research writer', "SEO specialist", "Software developer","Data analyst", "Digital marketer"],
-  loop: {},
-  typeSpeed: 100,
-  deleteSpeed: 40
-})
-  
+  const [typeEffect] = useTypewriter({
+    words: [
+      "Medical Doctor",
+      "Research writer",
+      "SEO specialist",
+      "Software developer",
+      "Data analyst",
+      "Digital marketer",
+    ],
+    loop: {},
+    typeSpeed: 100,
+    deleteSpeed: 40,
+  });
+
   return (
     <Container className="mt-9">
       <div className="max-w-2xl">
         <p className="text-3xl font-bold tracking-wide text-zinc-800 sm:text-5xl dark:text-zinc-100">
-        👋 Hello there! I'm a {''}
-
-          <span className="text-5xl tracking-wide text-[#66cdaa]">{typeEffect}</span>
+          👋 Hello there! I'm a&nbsp;
+          <span className="text-5xl tracking-wide text-[#66cdaa]">
+            {typeEffect}
+          </span>
           <span className="text-[#66cdaa]">
-
-          <Cursor/>
+            <Cursor />
           </span>
         </p>
         <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
-        Multitalented professional with a unique blend of medical expertise, technical prowess and creative flair. A medical doctor specializing in public health, coupled with extensive experience as a research writer, SEO specialist, software developer, data analyst, and digital marketer. Excelling in bridging the gap between healthcare, technology, and effective communication strategies
+          Multitalented professional with a unique blend of medical expertise,
+          technical prowess and creative flair. A medical doctor specializing in
+          public health, coupled with extensive experience as a research writer,
+          SEO specialist, software developer, data analyst, and digital
+          marketer. Excelling in bridging the gap between healthcare,
+          technology, and effective communication strategies
         </p>
       </div>
     </Container>
