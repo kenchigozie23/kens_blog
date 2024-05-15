95588a95e31033429cd22097d11888aea786a057
Ken Chigozie <kenronkwo@gmail.com>
2024-05-15 01:09:15 +0100

    blog

--- src/app/uses/page.tsx
+++ src/app/uses/page.tsx
@@ -54,7 +54,7 @@ const UsesPage = () => {
           </label>
           <textarea
             id="message"
-            rows="4"
+            rows={4}
             className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-teal-400"
             placeholder="Enter your message"
           ></textarea>
